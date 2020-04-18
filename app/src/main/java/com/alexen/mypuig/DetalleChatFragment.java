5d5b3c9df9e009548ac7ae6ef9f9f3c1d14a421b
ALEXEN <alexpf49@gmail.com>
2020-04-18 19:26:35 +0200

    agregar foto a chat listo

--- app/src/main/java/com/alexen/mypuig/DetalleChatFragment.java
+++ app/src/main/java/com/alexen/mypuig/DetalleChatFragment.java
@@ -1,11 +1,14 @@
 package com.alexen.mypuig;
 
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.navigation.NavController;
@@ -13,19 +16,25 @@ import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.VideoView;
 
 import com.alexen.mypuig.model.Chat;
-import com.alexen.mypuig.model.Notice;
 import com.alexen.mypuig.viewmodel.ChatViewModel;
 
+import java.util.EmptyStackException;
 import java.util.List;
+
+import static android.app.Activity.RESULT_OK;
 
 
 /**
@@ -36,8 +45,17 @@ public class DetalleChatFragment extends Fragment {
     ChatViewModel chatViewModel;
     NavController navController;
     MensajeAdapter mensajeAdapter;
-    private ImageButton sendButton;
+    private ImageButton sendButton, cameraButton, galleryButton;
     private EditText editTextMensaje;
+
+    public MutableLiveData<TipoMSG> estadoDelMSG = new MutableLiveData<>();
+
+    public enum TipoMSG {
+        INITIAL,
+        IMAGE,
+        TEXT,
+        VIDEO
+    }
 
     public DetalleChatFragment() {
         // Required empty public constructor
@@ -58,15 +76,24 @@ public class DetalleChatFragment extends Fragment {
         chatViewModel = ViewModelProviders.of(requireActivity()).get(ChatViewModel.class);
         navController = Navigation.findNavController(view);
 
+        galleryButton = view.findViewById(R.id.imageButtonClip);
+        cameraButton = view.findViewById(R.id.imageButtonCamera);
 
         sendButton = view.findViewById(R.id.imageButtonSend);
         editTextMensaje = view.findViewById(R.id.editTextMensaje);
 
+        galleryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                abrirGaleria();
+            }
+        });
+
         sendButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                estadoDelMSG.setValue(TipoMSG.TEXT);
                 chatViewModel.insertarMensajeChat(editTextMensaje.getText().toString());
-                Toast.makeText(getContext(),"HOBBÑUI",Toast.LENGTH_SHORT).show();
             }
         });
 
@@ -99,9 +126,19 @@ public class DetalleChatFragment extends Fragment {
             final Chat chat = chats.get(position);
 
 //            holder.nombreTextView.setText(chat.getMensajeAjeno());
-            holder.mensajeTextView.setText(chat.getMensajePropio());
-            holder.temaTextView.setText("aslnenasofe");
-            holder.fechaTextView.setText("vdrnñsnñrv");
+
+            if (estadoDelMSG.getValue()== TipoMSG.IMAGE){
+                holder.imageViewPreview.setImageURI(Uri.parse(chat.getMensajePropio()));
+                Toast.makeText(getContext(),"Imagen enviado",Toast.LENGTH_SHORT).show();
+            }else if (estadoDelMSG.getValue()== TipoMSG.VIDEO){
+                holder.videoViewPreview.setVideoURI(Uri.parse(chat.getMensajePropio()));
+                Toast.makeText(getContext(),"Video enviado",Toast.LENGTH_SHORT).show();
+            }else if (estadoDelMSG.getValue()== TipoMSG.TEXT){
+                holder.mensajeTextView.setText(chat.getMensajePropio());
+                Toast.makeText(getContext(),"Texto enviado",Toast.LENGTH_SHORT).show();
+            }
+
+
 
 //            holder.itemView.setOnClickListener(new View.OnClickListener() {
 //                @Override
@@ -124,14 +161,34 @@ public class DetalleChatFragment extends Fragment {
 
         class ChatViewHolder extends RecyclerView.ViewHolder {
             TextView mensajeTextView, temaTextView, fechaTextView;
-
+            ImageView imageViewPreview;
+            VideoView videoViewPreview;
             public ChatViewHolder(@NonNull View itemView) {
                 super(itemView);
                 mensajeTextView = itemView.findViewById(R.id.textViewMensajePropio);
                 temaTextView = itemView.findViewById(R.id.textViewTituloChat);
                 fechaTextView = itemView.findViewById(R.id.textViewFechaChat);
-
+                imageViewPreview = itemView.findViewById(R.id.imageViewPreviewMSG);
+                videoViewPreview = itemView.findViewById(R.id.videoViewPreviewMSG);
             }
+        }
+    }
+
+
+
+    public void abrirGaleria(){
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        intent.setType("image/");
+        startActivityForResult(Intent.createChooser(intent,"Seleccione la aplicacion"),10);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode==RESULT_OK){
+            Uri path = data.getData();
+            estadoDelMSG.setValue(TipoMSG.IMAGE);
+            chatViewModel.insertarMensajeChat(String.valueOf(path));
         }
     }
 }
